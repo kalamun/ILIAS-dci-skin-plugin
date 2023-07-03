77ed1147a82601aa73f508b92ca00cce8f4233e5
Kalamun <info@kalamun.org>
2023-07-03 18:26:26 +0000

    Add accordion progress

--- classes/class.ilDciSkinUIHookGUI.php
+++ classes/class.ilDciSkinUIHookGUI.php
@@ -92,15 +92,11 @@ class ilDciSkinUIHookGUI extends ilUIHookPluginGUI {
         foreach ($finder->query('//div[contains(@class, "ilc_va_cntr_VAccordCntr")]') as $node) {
           $node->setAttribute('class', $node->getAttribute('class') . ' dci-accordion');
           
-          // toggle
-          // $toggle = $finder->query('//div[contains(@class, "il_VAccordionToggleDef")]', $node)[0];
-          // $toggle->setAttribute('class', 'dci-accordion-toggle');
-          
           // heading
           $heading_wrapper = $finder->query('.//div[contains(@class, "ilc_va_ihead_VAccordIHead")]', $node)[0];
           $heading_wrapper->setAttribute('class', 'dci-accordion-heading');
-
           $heading = $finder->query('.//div', $heading_wrapper)[0];
+
           if ($heading) {
             $h2 = $dom->createElement('h2', htmlentities($heading->textContent));
             while ($heading->hasChildNodes()) {
@@ -108,10 +104,25 @@ class ilDciSkinUIHookGUI extends ilUIHookPluginGUI {
             }
             $heading->appendChild($h2);
             
+            // progress
+            $progress_total = count($finder->query('.//div[contains(@class, "kalamun-card_progress")]', $node));
+            $progress_completed = count($finder->query('.//div[contains(@class, "kalamun-card_progress completed")]', $node));
+            if ($progress_total > 0) {
+              $is_completed = $progress_completed >= $progress_total;
+
+              $progress_icon = $dom->createElement('span');
+              $progress_icon->setAttribute('class', 'icon-done');
+              $progress = $dom->createElement('div', $progress_completed . "/" . $progress_total);
+              $progress->setAttribute('class', 'accordion-progress' . ($is_completed ? ' completed' : ' not-completed'));
+              if ($is_completed) $progress->insertBefore($progress_icon, $progress->firstChild);
+              $heading->appendChild($progress);
+            }
+            
+            // toggle
             $toggle = $dom->createElement('div');
-            $toggle->setAttribute('class', 'icon-down');
+            $toggle->setAttribute('class', 'icon-down accordion-toggle');
             $heading->appendChild($toggle);
-          }         
+          }
         }
 
         $html = $dom->saveHTML();
