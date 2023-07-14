eca2cc1bce1e88ad2fd5257ad63fc8dc587b7fcb
Kalamun <info@kalamun.org>
2023-07-14 10:50:33 +0000

    Add body classes

--- inc/layout.php
+++ inc/layout.php
@@ -3,14 +3,26 @@
  * Generic layout functions
  */
 
-class dciSkin_layout {
+require_once __DIR__ . "/tabs.php";
 
-    public static function apply_custom_placeholders($html) {
+class dciSkin_layout
+{
+
+    public static function apply_custom_placeholders($html)
+    {
+        $body_class = [];
+
+        if (dciSkin_tabs::getRootCourse($_GET['ref_id']) !== false) {
+            $body_class[] = "is_course";
+        }
+
+        $html = str_replace("{BODY_CLASS}", implode(" ", $body_class), $html);
         $html = str_replace("{SKIN_URI}", "/Customizing/global/skin/dci", $html);
         return $html;
     }
 
-    public static function remove_default_cards($html) {
+    public static function remove_default_cards($html)
+    {
         $dom = new DomDocument();
         $internalErrors = libxml_use_internal_errors(true);
         $dom->loadHTML('<?xml encoding="utf-8" ?>' . $html);
@@ -20,9 +32,9 @@ class dciSkin_layout {
         // remove card container
         $card_container = $finder->query('//div[contains(@class, "ilContainerBlock")]');
         if (!empty($card_container[0])) {
-          $card_container[0]->parentNode->removeChild($card_container[0]);
+            $card_container[0]->parentNode->removeChild($card_container[0]);
         }
-        
+
         return str_replace('<?xml encoding="utf-8" ?>', "", $dom->saveHTML());
     }
 
