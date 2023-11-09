f5ff3c86f353640a931e7ebc8c4ef268182cce32
kalamun <rp@kalamun.net>
2023-11-09 14:54:52 +0000

    Debug

--- inc/course.php
+++ inc/course.php
@@ -56,7 +56,7 @@ class dciCourse {
         if($mandatory) {
             $sql = "SELECT * FROM `ut_lp_settings` WHERE obj_id=" . $ilDB->quote($course_id, "integer") . " AND obj_type='crs' LIMIT 1";
             $result = $ilDB->query($sql);
-
+var_dump($sql, $ilDB->numRows($result)); die();
             if ($ilDB->numRows($result) == 0) {
                 $sql = "INSERT INTO `ut_lp_settings` (obj_id, obj_type, u_mode, visits) "
                         ." VALUES(" . $ilDB->quote($course_id, "integer") . ", 'crs', " . $ilDB->quote(ilLPObjSettings::LP_MODE_COLLECTION) . ", 0)";
