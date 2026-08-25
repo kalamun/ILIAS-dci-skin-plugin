fbd3686e9dacbc701b04aef4327c415e5430e110
kalamun <rp@kalamun.net>
2026-08-25 11:50:45 +0200

    Extend accordion support to vertical

--- classes/class.ilDciSkinUIHookGUI.php
+++ classes/class.ilDciSkinUIHookGUI.php
@@ -201,7 +201,7 @@ class ilDciSkinUIHookGUI extends ilUIHookPluginGUI
                 /* accordion */
                 if ($a_part == "template_get" && isset($a_par["tpl_id"])
                     && $a_par["tpl_id"] == "components/ILIAS/COPage/tpl.page.html"
-                    && strpos($html, "ilc_va_icntr_AccordICntr") !== false) {
+                    && (strpos($html, "ilc_va_icntr_AccordICntr") !== false || strpos($html, "ilc_va_icntr_VAccordICntr") !== false)) {
                     $html = dciSkin_accordion::apply($html);
                 }
 
